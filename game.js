820c4ea59154b8b1a2027a412804c52b3786429b
rlapo213 <rlapo@naver.com>
2019-09-04 16:56:25 +0900

    Created System and TrackingBullet

--- game.js
+++ game.js
@@ -65,7 +65,7 @@ class Player{
         this.sprite = game.add.sprite(400,800,"player");
         this.sprite.anchor.setTo(0.5,0.5)
         game.physics.arcade.enable(this.sprite)
-        this.moveSpeed = 1000;
+        this.moveSpeed = 1300;
         
     }
     update(){
@@ -82,6 +82,7 @@ class Bullet{
         this.sprite = game.add.sprite(x,y,type)
         game.physics.arcade.enable(this.sprite)
         this.target = target
+        bullets.push(this);
     }
     setAngle(){
         this.sprite.rotation = game.physics.arcade.angleBetween(this.sprite,this.target)
@@ -92,6 +93,14 @@ class Bullet{
     update(){
         this.move();
     }
+    despawn(){
+        this.sprite.destroy();
+        let idx = bullets.findIndex(x=>x==this)
+        if(idx != -1){
+            bullets.splice(idx,1)
+        }
+        
+    }
 }
 class NormalBullet extends Bullet{
     constructor(x,y,target){
@@ -99,9 +108,10 @@ class NormalBullet extends Bullet{
         this.setAngle();
     }
     move(){
-        this.setAngle();
-
-        game.physics.arcade.velocityFromAngle(this.sprite.angle,300,this.sprite.body.velocity);
+        game.physics.arcade.velocityFromAngle(this.sprite.angle,400,this.sprite.body.velocity);
+        if((this.sprite.x < 0 || this.sprite.x > game.world.width) || (this.sprite.y < 0 || this.sprite.y > game.world.height)){
+            this.despawn();
+        }
     }
 }
 
@@ -109,49 +119,87 @@ class TrackingBullet extends Bullet{
     constructor(x,y,target){
         super(x,y,"TrackingBullet",target)
         this.setAngle();
+        this.killTime = game.time.now + 5000;
     }
     move(){
         this.setAngle();
-        game.physics.arcade.velocityFromAngle(this.sprite.angle,100,this.sprite.body.velocity);
+        game.physics.arcade.velocityFromAngle(this.sprite.angle,200,this.sprite.body.velocity);
+        if(game.time.now>= this.killTime){
+            this.despawn();
+        }
+    }
+}
+
+class System{
+    constructor(){
+        this.spawnTimer = game.time.create(false);
+        this.spawnTimer.loop(3000, this.randomSpawn,this);
+    }
+    start(){
+        this.spawnTimer.start();
+    }
+    randomSpawn(){
+        for(let i=0; i<game.rnd.between(3,7); i++){
+            let rx = game.rnd.between(0, game.world.width);
+            let rtype = game.rnd.between(0,1) ? "NormalBullet" : "TrackingBullet";
+            this.spawnBullet(rx,rtype)
+        }
+    }
+    spawnBullet(x,type){
+        if(type == "NormalBullet"){
+            new NormalBullet (x,0,player.sprite);
+        }
+        else if(type == "TrackingBullet"){
+            new TrackingBullet(x,0,player.sprite);
+        }
+    }
+    update(){
+        for(let i=0; i<bullets.length; i++){
+            if(bullets[i].sprite.alive){
+            bullets[i].update();
+            }
+        }
     }
 }
 
 var player;
-var bullet;
+var bullets=[];
+var system;
 
 
 var play = {
     preload: function () {
         game.load.image("player", "image.png");
-        game.load.image("NormalBullet","bullet.png");
+        game.load.image("NormalBullet","Normalbullet.png");
         game.load.image("TrackingBullet","trackingbullet.png");
 
     },
     create: function () {
         game.scale.pageAlignHorizontally=true;
         game.scale.pageAlignVertically=true;
-        game.stage.backgroundColor="#66bbaa"
+        game.stage.backgroundColor="#ffffff"
 
         player = new Player();
-        bullet = new NormalBullet(300,300,player.sprite)
-        
-
+        system = new System();
+        system.start();
 
 
     },
     update : function() {
         player.update();
-        bullet.update();
+        system.update();
         collider();
     }
 }
 
 function collision(player,bullet){
-    bullet.destory();
+    bullet.destroy();
 }
 
 function collider(){
-    game.physics.arcade.overlap(player.sprite, bullet.sprite,null,this)
+    for(let i =0; i<bullets.length; i++){
+        game.physics.arcade.overlap(player.sprite, bullets[i].sprite,collision,null,this);
+    }
 }
 
 game.state.add("Play", play)
